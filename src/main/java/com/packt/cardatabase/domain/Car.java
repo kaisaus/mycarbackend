a0c4b6c2bc6211967d25a6ea43f7a5f5f4a90c1c
Song Pham <song20002005@yahoo.com>
2022-08-20 19:13:13 +0700

    add source

---
+++ src/main/java/com/packt/cardatabase/domain/Car.java
@@ -0,0 +1,96 @@
+package com.packt.cardatabase.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String brand, model, color, registerNumber;
+    private int year, price;
+    
+    // the lazy strategy means that when the owner is fetched from the database, all the cars associated with the
+    // owner will be fetched when needed
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner")
+    private Owner owner;
+    
+    public Car() {
+    }
+    
+    public Car(String brand, String model, String color, String registerNumber, int year, int price, Owner owner) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registerNumber = registerNumber;
+        this.year = year;
+        this.price = price;
+        this.owner = owner;
+    }
+    
+    public long getId() {
+        return id;
+    }
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    
+    public void setModel(String model) {
+        this.model = model;
+    }
+    
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+    
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+    public String getBrand() {
+        return brand;
+    }
+    
+    public String getModel() {
+        return model;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+    
+    public String getRegisterNumber() {
+        return registerNumber;
+    }
+    
+    public int getYear() {
+        return year;
+    }
+    
+    public int getPrice() {
+        return price;
+    }
+    
+    //Getter and setter
+    public Owner getOwner() {
+        return owner;
+    }
+    
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+}
